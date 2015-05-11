2c11ad643fa96a0077a4eefca5f8486ce686c905
Luke <lukedupont@yahoo.com>
2015-05-10 22:59:57 -0500

    Updated background again

--- Blockoid/src/blockoid/states/playstate/world/Background.java
+++ Blockoid/src/blockoid/states/playstate/world/Background.java
@@ -10,13 +10,17 @@ public class Background {
 	//Color base1 = new Color(169,222,193);
 	Color base1 = new Color(169-16,222-16,193-16);
 	Color base2 = new Color(109,201,180);
+	Star[] stars = new Star[64];
 	
 	public Background(World world) {
 		this.world = world;
+		for(int i = 0; i < stars.length; i++) {
+			stars[i] = new Star(world.game);
+		}
 	}
 	
 	public void draw(Graphics2D g) {
-		int lightLevel = world.lightLevels[world.hour];
+		int lightLevel = world.sunlightLevel;
 		//Atmosphere
 		int x = 0;
 		int y = 0;
@@ -24,38 +28,49 @@ public class Background {
 		int ySize;
 		//g.setColor(atmosphere);
 		//g.fillRect(x, y, xSize, ySize);
-		int interval = 255/7;
 		
-		Color clr = modify(atmosphere, -(7-lightLevel)*interval/2, -(7-lightLevel)*interval/2, -(7-lightLevel)*interval/2);
-		for(int i = 4; i >= 0; i--) {
+		int interval = 255/7;
+		System.out.println("At atmos: " + lightLevel);
+		Color clr = modify(atmosphere, -(7-lightLevel)*interval, -(7-lightLevel)*interval, -(7-lightLevel)*interval);
+		for(int i = 5; i >= 0; i--) {
+			
 			int dy = i*(world.game.height/10);
 			int dx = 0;
 			ySize = world.game.height/9;
+			if(i==5) ySize = ySize + world.game.height/2;
 			xSize = world.game.width;
 			g.setColor(clr);
 			g.fillRect(dx, dy, xSize, ySize);
 			//clr = clr.darker();
 			clr = modify(clr, -lightLevel, -lightLevel, 0);
 		}
+		System.out.println("At stars: " + lightLevel);
+		if(lightLevel < 3.9) {
+			if(lightLevel >= 3) {g.setColor(new Color(255,255,255,55));} else {g.setColor(Color.WHITE);}
+			for(int i = 0; i < stars.length; i++) {
+				
+				stars[i].draw(g);
+			}
+		}
 		
 		//Background
-		x = 0;
-		y = (world.game.height/2)-1;
-		xSize = world.game.width;
-		ySize = world.game.height/2;
-		g.setColor(modify(base1,-16,-16,-16));
-		g.fillRect(x, y, xSize, ySize);
+		//x = 0;
+		//y = (world.game.height/2)-1;
+		//xSize = world.game.width;
+		//ySize = world.game.height/2;
+		//g.setColor(modify(base1,-16,-16,-16));
+		//g.fillRect(x, y, xSize, ySize);
 		
 		//Foreground
-		x = 0;
-		y = (world.game.height/2)+1;
-		xSize = world.game.width;
-		ySize = world.game.height/2;
-		g.setColor(base1);
-		g.fillRect(x, y, xSize, ySize);
+		//x = 0;
+		//y = (world.game.height/2)+1;
+		//xSize = world.game.width;
+		//ySize = world.game.height/2;
+		//g.setColor(base1);
+		//g.fillRect(x, y, xSize, ySize);
 		
-		g.setColor(new Color(0,0,0,bound((7-lightLevel)*interval,0,255)));
-		g.fillRect(0, 0, world.game.width, world.game.height);
+		//g.setColor(new Color(0,0,0,bound((7-lightLevel)*interval,0,255)));
+		//g.fillRect(0, 0, world.game.width, world.game.height);
 	}
 	
 	
